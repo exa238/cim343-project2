66436581c942fe87b18cf7c07fb135048903c3c4
exa238 <exa783@miami.edu>
2026-03-31 21:18:19 -0400

    added final javascript changes and updated html and css

--- severance-styling.js
+++ severance-styling.js
@@ -1,38 +1,59 @@
 "use strict";
 // change the name when a person enters their own
-window.onload = function (){
-    const greeting =  document.getElementById("greeting");
+window.onload = function () {
+    const greeting = document.getElementById("greeting");
+    let savedName = localStorage.getItem("username");
 
-    let name = prompt("Who are you?")
-
-    if(name && name.trim() !== ""){
-        greeting.textContent = "Welcome, " + name.trim();
-    } 
-    else{
-        greeting.textContent = "Welcome, Employee";
+    if (savedName) {
+        greeting.textContent = "Welcome, " + savedName;
+        return;
     }
+    let name = prompt("Who are you?");
 
-}
+    if (name && name.trim() !== "") {
+        name = name.trim();
+        greeting.textContent = "Welcome, " + name;
+        localStorage.setItem("username", name);
+    }else{
+        greeting.textContent = "Welcome, Employee";
+        localStorage.setItem("username", "Employee");
+    }
+};
 
 const nightBtn = document.getElementById("nightModeBtn");
 
 // save even though it loads
 if (localStorage.getItem("darkMode") === "enabled") {
     document.body.classList.add("dark-mode");
-    nightBtn.textContent = "Deactivate MDR Mode";
+    nightBtn.textContent = "View in the eyes of man";
 }
-
 nightBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-
     if (document.body.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "enabled");
-        nightBtn.textContent = "Deactivate MDR Mode";
+        nightBtn.textContent = "View in the eyes of man";
     } else {
         localStorage.setItem("darkMode", "disabled");
-        nightBtn.textContent = "Activate MDR Mode";
+        nightBtn.textContent = "View in the eyes of Kier";
     }
 });
+
+let textButton = document.getElementById("text-button");
+
+textButton.addEventListener("click", () =>{
+    document.body.style.fontSize = "24px";
+    text.innerHTML = "Text size increased!";
+});
+
+let revertButton = document.getElementById("revert-button");
+
+revertButton.addEventListener("click", () =>{
+    document.body.style.fontSize = "16px";
+    text.innerHTML = "Text size reverted!";
+});
+
+
+
 
 
 
